707d81c77dd9b255d2ead21d91405814de69a253
Ashiq Mohamed <ashiqmoh@gmail.com>
2017-08-25 19:47:49 +0200

    added a few basic types of typescript

---
+++ basic-type.ts
@@ -0,0 +1,13 @@
+// declaring boolean
+let isDone: boolean = false;
+
+// declaring number (all numbers in TypeScript is floating point values)
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// declaring string with :string
+// either with single-quote ' or double-quote "
+let color: string = "blue";
+color = "red";
